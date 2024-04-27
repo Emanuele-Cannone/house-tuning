cd1ad2e8e7bb6f7ea877c7f6d0aaa7f8f51164f1
Emanuele <cannone.emanuele@gmail.com>
2024-04-27 15:02:19 +0200

    change Vehicle.php

--- app/Models/Vehicle.php
+++ app/Models/Vehicle.php
@@ -4,12 +4,24 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Vehicle extends Model
 {
     use HasFactory;
 
+    /**
+     * @var string[]
+     */
     protected $fillable = [
         'name'
     ];
+
+    /**
+     * @return HasMany
+     */
+    public function vehicleBrands(): HasMany
+    {
+        return $this->hasMany(VehicleBrand::class);
+    }
 }
